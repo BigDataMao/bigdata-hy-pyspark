03055b5602febafa710c3e6f42ee36c3a266d85a
毛新伟 <mxw712@gmail.com>
2024-09-01 16:40:41 +0800

    feat: 获取read和write全部的表名

--- src/etl/dwd/get_table_name.py
+++ src/etl/dwd/get_table_name.py
@@ -6,7 +6,10 @@ from collections import defaultdict
 
 def find_spark_tables(directory):
     table_names = defaultdict(list)  # 使用字典分类表名
-    pattern = re.compile(r'spark\.table\("([^"]+)"\)')  # 正则表达式匹配表名
+    # 正则表达式匹配表名格式为 spark.table("表名")
+    pattern_read = re.compile(r'spark\.table\("([^"]+)"\)')
+    # 正则表达式匹配表名格式为 return_to_hive(..., target_table="表名"...)
+    pattern_write = re.compile(r'.*target_table\s*=\s*"([^"]+)"')
 
     # 遍历目录下的所有文件
     for root, dirs, files in os.walk(directory):
@@ -15,9 +18,11 @@ def find_spark_tables(directory):
                 file_path = os.path.join(root, file)
                 with open(file_path, 'r', encoding='utf-8') as f:
                     content = f.read()
-                    matches = pattern.findall(content)  # 查找所有匹配的表名
-                    for match in matches:
-                        print(f"Found table: {match}")
+                    matches_read = pattern_read.findall(content)  # 查找所有匹配的表名
+                    matches_write = pattern_write.findall(content)
+                    print("matches_write: ", matches_write)
+                    matches_total = matches_read + matches_write
+                    for match in matches_total:
                         # 根据表名中间的部分进行分类
                         parts = match.split('.')
                         if len(parts) >= 2:  # 确保有点分隔
@@ -29,10 +34,13 @@ def find_spark_tables(directory):
 
 if __name__ == "__main__":
     current_directory = os.getcwd()  # 获取当前目录
-    categorized_tables = find_spark_tables(current_directory)
+    # 获取上级目录下的ads目录
+    ads_directory = os.path.join(current_directory, '..', 'ads')
+    print(f"Searching for Spark tables in: {ads_directory}")
+    categorized_tables = find_spark_tables(ads_directory)
 
     # 打印分类后的表名
     for category, tables in categorized_tables.items():
         print(f"Category: {category}")
         for table in set(tables):  # 去重后打印
-            print(f"  - {table}")
+            print(f"    {table}")
