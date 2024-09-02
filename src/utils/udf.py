227c480e65957170fcfcbe85aa62d78e4f0ebf2c
samuel <mxw712@gmail.com>
2024-09-02 15:39:28 +0800

    fix: 微调udf

--- src/utils/udf.py
+++ src/utils/udf.py
@@ -11,9 +11,14 @@ def f_get_multi_key_note(i_key_value: str, i_key_name: str):
     """
     key_value_list = i_key_value.split(',')
     note_list = [map_table.get(i_key_name).get(key) for key in key_value_list]
+    # 去除空值
+    note_list = [note for note in note_list if note]
     return ','.join(note_list)
 
 
 if __name__ == '__main__':
+    # 用于测试,打印结果应该是: "C4"
+    print(f_get_multi_key_note('3', 'report.wh_risk_level'))
+
     # 用于测试,打印结果应该是: "C2,C3,C4"
     print(f_get_multi_key_note('1,2,3', 'report.wh_risk_level'))
