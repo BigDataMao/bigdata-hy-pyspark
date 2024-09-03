9d95283a7c14551467034d9c8642ca35a4b170e6
samuel <mxw712@gmail.com>
2024-09-03 13:19:14 +0800

    style: 修改大小写

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ from src.etl.ads.p_cockpit_00128_data import p_cockpit_00128_data
 from src.etl.ads.p_cockpit_00140_data import p_cockpit_00140_data
 from src.etl.ads.p_cockpit_00156_data import p_cockpit_00156_data
 from src.etl.ads.p_cockpit_00208_data import p_cockpit_00208_data
-from src.etl.dwd.P_COCKPIT_CLIENT_REVENUE import p_cockpit_client_revenue
+from src.etl.dwd.p_cockpit_client_revenue import p_cockpit_client_revenue
 from src.utils.logger_uitls import to_color_str
 
 busi_date = parse_args()
@@ -17,7 +17,7 @@ spark = create_env()
 
 if __name__ == '__main__':
     begin_time = time.time()
-    # p_cockpit_client_revenue(spark, busi_date)
+    p_cockpit_client_revenue(spark, busi_date)
 
     # TODO 请实验下缓存上面的数据，然后在下面的函数中使用
     p_cockpit_00114_data(spark, busi_date)
